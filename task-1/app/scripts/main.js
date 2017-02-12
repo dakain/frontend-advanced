932ae528ef5aa315d17f2582163eba4d78320a0c
Oleksandr Tkachuk <gdakain@gmail.com>
2017-02-12 16:30:42 +0200

    Completed task-1.

---
+++ task-1/app/scripts/main.js
@@ -0,0 +1,15 @@
+console.log('\'Allo \'Allo!');
+
+var comments = [
+  {
+    author: 'Tkachuk',
+    body: 'Privet, kak deal?',
+    comments: [
+      {
+        author: 'Zubriychuk',
+        body: 'Ta norm dela, kak sam?',
+        comments: []
+      }
+    ]
+  }
+];
